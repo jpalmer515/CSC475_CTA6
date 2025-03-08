ed8b1e61b50bc25a04b8d76ff14018a6b9308c0e
jakepalmer <jpalmer515@gmail.com>
2025-03-07 18:06:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/csc45_cta6_portfoliomilestone/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.csc45_cta6_portfoliomilestone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.csc45_cta6_portfoliomilestone.ui.theme.CSC45_CTA6_PortfolioMilestoneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CSC45_CTA6_PortfolioMilestoneTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CSC45_CTA6_PortfolioMilestoneTheme {
+        Greeting("Android")
+    }
+}
